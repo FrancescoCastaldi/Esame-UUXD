2f9d50b4083eeeb70f7b6a36df7ce206d017cb29
stefmercu <stef39108@gmail.com>
2026-06-26 11:19:16 +0200

    R4: aggiunti tip descrittivi a tutti i passi dei moduli trainer (Viaggio, Ricarica, Rinnovo, Biglietto)

--- Website/js/trainer-modules.js
+++ Website/js/trainer-modules.js
@@ -27,6 +27,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Scegli il canale di acquisto",
         "desc": "Due opzioni: ONLINE su solweb.tper.it (rapido, paghi con carta o PayPal, ricevi tutto via email) oppure FISICO presso un Punto Tper (paghi in contanti o carta, ritiri subito). Clicca sotto per vedere i canali dedicati.",
         "highlight": "#buy-channels",
+        "tip": "Scegli tra acquisto online su solweb o di persona al Punto Tper.",
         "action": {
           "textKey": "trainer.goto-channels",
           "url": "dove-comprare.html#punti-tper"
@@ -61,6 +62,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Inserisci la partenza",
         "desc": "Scrivi il tuo indirizzo, una fermata o il nome di una via nel campo 'Partenza'. Il sistema ti suggerir\u00e0 le opzioni mentre scrivi.",
         "highlight": "#from",
+        "tip": "Digita qui il punto di partenza: indirizzo, via o nome della fermata.",
         "hint": true,
         "action": {
           "textKey": "trainer.goto-pianifica",
@@ -72,6 +74,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Inserisci la destinazione",
         "desc": "Ora scrivi dove vuoi arrivare nel campo 'Destinazione'. Puoi usare nomi di vie, fermate, monumenti o quartieri.",
         "highlight": "#to",
+        "tip": "Digita qui la destinazione: indirizzo, via, monumento o quartiere.",
         "hint": true,
         "action": {
           "textKey": "trainer.goto-pianifica",
@@ -83,6 +86,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Scegli la modalit\u00e0 di trasporto",
         "desc": "Seleziona il mezzo che preferisci: bus, treno, bici o piedi. Puoi anche lasciare 'Tutti' per vedere tutte le opzioni disponibili.",
         "highlight": "#travel-mode",
+        "tip": "Scegli il mezzo di trasporto: bus, treno, bici o a piedi.",
         "action": {
           "textKey": "trainer.goto-pianifica",
           "url": "pianifica.html"
@@ -93,6 +97,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Cerca il percorso",
         "desc": "Premi il pulsante 'Cerca' per avviare la ricerca. Il sistema mostrer\u00e0 tutte le opzioni di percorso disponibili con tempi e durata.",
         "highlight": "#plan-route-btn",
+        "tip": "Clicca qui per cercare tutti i percorsi disponibili.",
         "hint": true,
         "action": {
           "textKey": "trainer.search-now",
@@ -104,6 +109,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Confronta i risultati",
         "desc": "Scegli il percorso pi\u00f9 adatto a te confrontando durata, numero di fermate e mezzo di trasporto. Il percorso pi\u00f9 veloce \u00e8 evidenziato.",
         "highlight": "#results-container",
+        "tip": "Qui appariranno i percorsi trovati. Confronta durata, fermate e mezzi.",
         "hint": true,
         "completionMessage": "Percorso trovato!"
       },
@@ -139,6 +145,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Acquista con l'App Roger (digitale)",
         "desc": "L'App Roger \u00e8 il canale pi\u00f9 comodo per biglietti singoli e carnet digitali. Scaricala sul tuo smartphone, registrati e acquista in pochi click. Il biglietto si attiva via GPS quando sali a bordo.",
         "highlight": "#roger",
+        "tip": "Scarica l'App Roger per acquistare biglietti digitali direttamente dal telefono.",
         "action": {
           "textKey": "trainer.goto-roger",
           "url": "dove-comprare.html#roger"
@@ -149,6 +156,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "In alternativa: canale fisico",
         "desc": "Puoi comprare biglietti cartacei in qualsiasi Punto Tper, tabaccheria convenzionata o edicola con PuntoLis. Paghi in contanti o carta.",
         "highlight": "#punti-tper",
+        "tip": "Trova il Punto Tper più vicino per acquistare biglietti cartacei.",
         "action": {
           "textKey": "trainer.goto-points",
           "url": "dove-comprare.html#punti-tper"
@@ -183,6 +191,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Ricaricare in tabaccheria / PuntoLis",
         "desc": "Vai in una tabaccheria o edicola con il simbolo PuntoLis. Consegna la Roger Card e chiedi una ricarica. Puoi ricaricare l'importo che preferisci. Il saldo viene aggiornato subito.",
         "highlight": "#tabaccherie",
+        "tip": "Cerca una tabaccheria o edicola con il simbolo PuntoLis per ricaricare.",
         "action": {
           "textKey": "trainer.goto-tabacchi",
           "url": "dove-comprare.html#tabaccherie"
@@ -193,6 +202,7 @@ window.INLINE_TRAINER_MODULES = {
         "title": "Ricaricare con App Roger",
         "desc": "Apri l'App Roger, vai alla sezione 'Ricarica', inserisci il numero della tua Roger Card e scegli l'importo. Il pagamento \u00e8 digitale e il saldo \u00e8 disponibile subito.",
         "highlight": "#roger",
+        "tip": "Apri l'App Roger sul telefono e vai alla sezione Ricarica.",
         "action": {
           "textKey": "trainer.goto-roger",
           "url": "dove-comprare.html#roger"
